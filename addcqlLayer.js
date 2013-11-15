6a7c2bb1dc38228c5d218770512f411dcee13a65
naal <nacho.althabe@gmail.com>
2013-11-15 10:43:59 -0300

    Fixes

--- addcqlLayer.js
+++ addcqlLayer.js
@@ -1,9 +1,10 @@
 var layerEscuelasCql = new OpenLayers.Layer.WMS("Escuelas Filtradas", "http://192.168.54.20/geoserver/mariano/wfs", {
-  layers: "mariano:v_escuelas_geoserver",
-  transparent:true,
-  cql_filter: 'sector = 20',
-  format: 'image/png'},
-  {isBaseLayer: false, visibility: false, ratio: 1,displayInLayerSwitcher: false,  group: "comun",singleTile: true
+    layers: "mariano:v_escuelas_geoserver",
+    transparent:true,
+    cql_filter: 'sector = 20',
+    format: 'image/png'
+  },{
+    isBaseLayer: false, visibility: false, group: "consulta"
 });
 var addCQLlayer = function(cql,ambito){
   layerEscuelasCql.mergeNewParams({
